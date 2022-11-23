bdbd814d117c020ead0544ea6b18b40b52f9ec0f
Melika Khodamoradi <m.11moradi@gmail.com>
2022-11-23 12:29:57 +0330

    add changes

---
+++ src/main/java/net/guides/springboot/notificationsystem/service/NotificationService.java
@@ -0,0 +1,22 @@
+package net.guides.springboot.notificationsystem.service;
+
+
+import net.guides.springboot.notificationsystem.model.Notif;
+import net.guides.springboot.notificationsystem.service.model.EmailModel;
+
+import java.util.List;
+
+public interface NotificationService {
+
+    Notif getByUserId(Long userId) ;
+
+    List<Notif> getAllNotifications() ;
+
+//  void save(String deviceId, String token, TokenType type, Long userId);
+
+    void save(Notif notif);
+
+    void sendEmail(EmailModel emailModel);
+
+
+}
